3b3a291f88ec7a6b4cef0280be1522709fb686f6
rixa65 <142931180+rixa65@users.noreply.github.com>
2025-02-01 07:12:13 +0100

    Sound Mixer Done Sort of but 0 don't work

---
+++ ThesisProject/Assets/TutorialProject/Scripts/AudioMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioMixer audioMixer;
+    [SerializeField] TextMeshProUGUI masterValueTxt;
+    [SerializeField] TextMeshProUGUI sfxValueTxt;
+    [SerializeField] TextMeshProUGUI ambienceValueTxt;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetMasterVolume(float volume)
+    {
+        int volumeToInt = (int)Mathf.Clamp((volume * 10),0,20);
+        masterValueTxt.text = volumeToInt.ToString();
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        int volumeToInt = (int)Mathf.Clamp((volume * 10), 0, 20);
+        sfxValueTxt.text = volumeToInt.ToString();
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+    public void SetAmbienceVolume(float volume)
+    {
+        int volumeToInt = (int)Mathf.Clamp((volume * 10), 0, 20);
+        ambienceValueTxt.text = volumeToInt.ToString();
+        audioMixer.SetFloat("AmbienceVolume", Mathf.Log10(volume) * 20);
+
+    }
+}
